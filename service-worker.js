0fa5cd62c37a5997e5b04079550ad233a76a776f
sdorpl <macszymk@outlook.com>
2020-05-11 11:25:27 +0200

    updates

--- service-worker.js
+++ service-worker.js
@@ -18,8 +18,7 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'excalc-alpha-0.02';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const CACHE_NAME = 'excalc-alpha-0.03';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -30,20 +29,7 @@ const FILES_TO_CACHE = [
   '/scripts/luxon-1.11.4.js',
   '/styles/inline.css',
   '/images/add.svg',
-  '/images/clear-day.svg',
-  '/images/clear-night.svg',
-  '/images/cloudy.svg',
-  '/images/fog.svg',
-  '/images/hail.svg',
   '/images/install.svg',
-  '/images/partly-cloudy-day.svg',
-  '/images/partly-cloudy-night.svg',
-  '/images/rain.svg',
-  '/images/refresh.svg',
-  '/images/sleet.svg',
-  '/images/snow.svg',
-  '/images/thunderstorm.svg',
-  '/images/tornado.svg',
   '/images/favicon.ico',
 ];
 
