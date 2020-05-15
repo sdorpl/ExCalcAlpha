2992a556dd3a42e763f76f99e198951fd9a7b579
sdorpl <macszymk@outlook.com>
2020-05-15 11:24:31 +0200

    updt

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@
 
 // CODELAB: Update cache names any time any of the cached files change.
 
-const CACHE_NAME = 'excalc-alpha-0.42';
+const CACHE_NAME = 'excalc-alpha-0.43';
 
 
 // CODELAB: Add list of files to cache here.
