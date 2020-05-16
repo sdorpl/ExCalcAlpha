022425caaedecd18d1980551c007b99aafc99ec0
sdorpl <macszymk@outlook.com>
2020-05-16 11:56:02 +0200

    icons

--- service-worker.js
+++ service-worker.js
@@ -24,7 +24,7 @@ workbox.googleAnalytics.initialize();
 
 // CODELAB: Update cache names any time any of the cached files change.
 
-const CACHE_NAME = 'excalc-alpha-0.60';
+const CACHE_NAME = 'excalc-alpha-0.65';
 
 
 // CODELAB: Add list of files to cache here.
@@ -73,25 +73,6 @@ evt.waitUntil(
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
 // CODELAB: Add fetch event handler here.
-
-if (evt.request.url.includes('/forecast/')) {
-  console.log('[Service Worker] Fetch (data)', evt.request.url);
-  evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-              return response;
-            }).catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-      }));
-  return;
-}
 
 evt.respondWith(
     caches.open(CACHE_NAME).then((cache) => {
